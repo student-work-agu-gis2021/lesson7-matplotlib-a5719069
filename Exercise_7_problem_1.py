2025d29db905c88ea3a43573715a0e4342ed30f8
a5719069 <>
2021-05-30 17:19:30 +0000

    The part related to color was modified to be represented by various colors.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -59,7 +59,7 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 # Plot a scatter plot
 # YOUR CODE HERE 3
 import matplotlib.pyplot as plt
-plt.scatter(x,y,s=50,c='black',cmap='rainbow',edgecolor='red')
+plt.scatter(x,y,s=50,c=colors ,cmap='rainbow',edgecolor='red')
 
 
 
